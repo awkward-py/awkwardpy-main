a4fffae0fcf3a534afe3af9ab556608eda5d3117
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-05-28 20:43:02 +0530

    Update hero-content.tsx

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -52,7 +52,7 @@ export const HeroContent = () => {
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Let start
+          Currently on working
         </motion.a>
       </div>
 
