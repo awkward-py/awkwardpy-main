4015af45caf6d9226c3169ee7715b169c8dfe301
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-07-30 20:33:28 +0530

    Update hero-content.tsx

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -52,7 +52,7 @@ export const HeroContent = () => {
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-         Built with Next js  
+         Built with Next.Js 14 
         </motion.a> 
       </div>
 
